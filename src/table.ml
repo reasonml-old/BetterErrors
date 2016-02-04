d8da11e34fab83515bd29069eb1006d821124973
Cheng Lou <chenglou92@gmail.com>
2016-02-04 08:01:00 -0500

    Add more features to ascii table
    Because I'm having fun.
    
    - Unicode/Ascii vertical&horizontal border. Means we need to use String
      instead of Char now.
    - Compact, borderless table through `None`.
    - Padding amount.
    - I need to sleep.

--- src/table.ml
+++ src/table.ml
@@ -3,8 +3,8 @@ type align =
   | Right
   | Center
 
-let pad ?(align=Left) content ~total =
-  let freeSpace = total - (BatString.length content) in
+let pad ~align ~totalWidth content =
+  let freeSpace = totalWidth - (BatString.length content) in
   match align with
   | Left ->
     content ^ (BatString.make freeSpace ' ')
@@ -16,42 +16,55 @@ let pad ?(align=Left) content ~total =
     let rightSpace = freeSpace - leftSpace in
     (BatString.make leftSpace ' ') ^ content ^ (BatString.make rightSpace ' ')
 
-let table l =
+let print_horizontal_border ~joint ~hBorder ~row ~maxes =
+  print_string joint;
+  BatList.iter2 (fun cell maxWidth ->
+    print_string @@ (BatString.repeat hBorder maxWidth) ^ joint
+  ) row maxes;
+  print_endline ""
+
+let table ?(align=Left) ?(hBorder=Some "-") ?(vBorder=Some "|") ?(padding=1) l =
   let columnLength = BatList.length (BatList.hd l) in
   let maxes = BatList.init columnLength (fun i ->
     let column = BatList.map (fun row ->
        BatString.length @@ BatList.nth row i
     ) l in
-    BatList.max column
+    padding * 2 + BatList.max column
   ) in
 
-  print_string "+";
-  for i = 0 to (BatList.length @@ BatList.hd l) - 1 do
-    let maxWidth = BatList.nth maxes i in
-    print_string @@ (BatString.make (maxWidth + 2) '-') ^ "+"
-  done;
-  print_endline "";
+  let vBorder2 = BatPervasives.(|?) vBorder "" in
+  let joint = match vBorder with
+  | None -> ""
+  | Some _ -> "+"
+  in
+  (* top border *)
+  (match hBorder with
+  | None -> ()
+  | Some ch -> print_horizontal_border ~joint ~hBorder:ch ~row:(BatList.hd l) ~maxes);
 
+  let paddingStr = BatString.make padding ' ' in
   BatList.iter (fun row ->
-    print_string "|";
+    print_string vBorder2;
     BatList.iter2 (fun cell maxWidth ->
-      print_string @@ (pad ~align:Left cell ~total:(maxWidth + 2)) ^ "|"
+      print_string @@
+        (* padding is added on top of totalWidth *)
+        (pad ~align ~totalWidth:maxWidth (paddingStr ^ cell ^ paddingStr)) ^ vBorder2
     ) row maxes;
 
-    print_string "\n+";
-    BatList.iter2 (fun cell maxWidth ->
-      print_string @@ (BatString.make (maxWidth + 2) '-') ^ "+"
-    ) row maxes;
-    print_endline ""
+    print_endline "";
+    (match hBorder with
+    | None -> ()
+    | Some ch -> print_horizontal_border ~joint ~hBorder:ch ~row ~maxes)
   ) l
 
-
 let () = table [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
-
-(*
-+---+---+---+
-| 1 | 2 | 3 |
-+---+---+---+
-| 4 | 5 | 6 |
-+---+---+---+
-*)
+let () = print_endline "\n"
+let () = table ~hBorder:(Some "=") ~vBorder:(Some "║") ~align:Right [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+let () = print_endline "\n"
+let () = table ~padding:0 ~align:Center [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+let () = print_endline "\n"
+let () = table ~hBorder:None ~align:Center [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+let () = print_endline "\n"
+let () = table ~vBorder:None [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+let () = print_endline "\n"
+let () = table ~vBorder:None ~hBorder:None ~align:Right [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
