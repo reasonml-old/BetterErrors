f4f3eaa06b632d1a4e861fa9bfd4def513d40040
Cheng Lou <chenglou92@gmail.com>
2016-02-05 02:28:31 -0500

    Better table options
    Use presets instead.

--- src/table.ml
+++ src/table.ml
@@ -6,65 +6,60 @@ type align =
 let pad ~align ~totalWidth content =
   let freeSpace = totalWidth - (BatString.length content) in
   match align with
-  | Left ->
-    content ^ (BatString.make freeSpace ' ')
-  | Right ->
-    (BatString.make freeSpace ' ') ^ content
+  | Left -> content ^ (BatString.make freeSpace ' ')
+  | Right -> (BatString.make freeSpace ' ') ^ content
   | Center ->
     let leftSpace = freeSpace / 2 in
     (* if freeSpace is odd, give rightSpace one more space *)
     let rightSpace = freeSpace - leftSpace in
     (BatString.make leftSpace ' ') ^ content ^ (BatString.make rightSpace ' ')
 
-let print_horizontal_border ~joint ~hBorder ~row ~maxes =
-  print_string joint;
-  BatList.iter2 (fun cell maxWidth ->
-    print_string @@ (BatString.repeat hBorder maxWidth) ^ joint
-  ) row maxes;
-  print_endline ""
+let drawHorizontalLine ~style:(left, mid, right, horizontal) ~row ~maxes =
+  if horizontal = "" then ""
+  else
+    BatList.map2 (fun cell maxWidth -> BatString.repeat horizontal maxWidth) row maxes
+    |> BatList.interleave ~first:left ~last:(right ^ "\n") mid
+    |> BatString.concat ""
 
-let table ?(align=Left) ?(hBorder=Some "-") ?(vBorder=Some "|") ?(padding=1) l =
-  let columnLength = BatList.length (BatList.hd l) in
-  let maxes = BatList.init columnLength (fun i ->
-    let column = BatList.map (fun row ->
-       BatString.length @@ BatList.nth row i
-    ) l in
-    padding * 2 + BatList.max column
-  ) in
+let column l i = BatList.map (fun row -> BatString.length @@ BatList.nth row i) l
 
-  let vBorder2 = BatPervasives.(|?) vBorder "" in
-  let joint = match vBorder with
-  | None -> ""
-  | Some _ -> "+"
+let simple =          ("┌", "┬", "┐", "├", "┼", "┤", "└", "┴", "┘", "─", "│")
+let double =          ("╔", "╦", "╗", "╠", "╬", "╣", "╚", "╩", "╝", "═", "║")
+let dotted =          (" ", " ", " ", " ", " ", " ", " ", " ", " ", "┄", "┆")
+let onlyVertical =    ("┌", "┬", "┐", "├", "┼", "┤", "└", "┴", "┘", "", "│")
+let onlyHorizontal =  (" ", "─", " ", " ", "─", " ", " ", "─", " ", "─", " ")
+let compact =         ("", "", "", "", "", "", "", "", "", "", "")
+let onlyIner =        (" ", " ", " ", "├", "┼", "┤", " ", " ", " ", "─", "│")
+
+let table ?(align=Left) ?(style=simple) ?(padding=1) lists =
+  let (topLeft, topMid, topRight, midLeft, midMid, midRight, bottomLeft,
+    bottomMid, bottomRight, hor, ver) = style in
+
+  let anyRow = BatList.hd lists in
+  let maxes = BatList.init
+    (BatList.length anyRow)
+    (fun i -> padding * 2 + BatList.max (column lists i))
   in
-  (* top border *)
-  (match hBorder with
-  | None -> ()
-  | Some ch -> print_horizontal_border ~joint ~hBorder:ch ~row:(BatList.hd l) ~maxes);
-
   let paddingStr = BatString.make padding ' ' in
-  BatList.iter (fun row ->
-    print_string vBorder2;
-    BatList.iter2 (fun cell maxWidth ->
-      print_string @@
-        (* padding is added on top of totalWidth *)
-        (pad ~align ~totalWidth:maxWidth (paddingStr ^ cell ^ paddingStr)) ^ vBorder2
-    ) row maxes;
+  (BatList.map (fun row ->
+    BatList.map2 (fun cell maxWidth ->
+      (* padding is added on top of totalWidth *)
+      pad ~align ~totalWidth:maxWidth (paddingStr ^ cell ^ paddingStr)
+    ) row maxes
+    |> BatList.interleave ~first:ver ~last:(ver ^ "\n") ver
+    |> BatString.concat ""
+  ) lists)
+  |> BatList.interleave
+    ~first: (drawHorizontalLine ~style:(topLeft, topMid, topRight, hor) ~row:anyRow ~maxes)
+    ~last: (drawHorizontalLine ~style:(bottomLeft, bottomMid, bottomRight, hor) ~row: anyRow ~maxes)
+    (drawHorizontalLine ~style:(midLeft, midMid, midRight, hor) ~row: anyRow ~maxes)
+  |> BatString.concat ""
 
-    print_endline "";
-    (match hBorder with
-    | None -> ()
-    | Some ch -> print_horizontal_border ~joint ~hBorder:ch ~row ~maxes)
-  ) l
-
-let () = table [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
-let () = print_endline "\n"
-let () = table ~hBorder:(Some "=") ~vBorder:(Some "║") ~align:Right [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
-let () = print_endline "\n"
-let () = table ~padding:0 ~align:Center [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
-let () = print_endline "\n"
-let () = table ~hBorder:None ~align:Center [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
-let () = print_endline "\n"
-let () = table ~vBorder:None [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
-let () = print_endline "\n"
-let () = table ~vBorder:None ~hBorder:None ~align:Right [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+let () = print_endline @@ table [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+let () = print_endline @@ table ~style:double ~align:Right [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+let () = print_endline @@ table ~style:dotted ~align:Center [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+let () = print_endline @@ table ~style:compact [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+let () = print_endline @@ table ~padding:0 ~align:Center [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+let () = print_endline @@ table ~style:onlyVertical [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+let () = print_endline @@ table ~style:onlyHorizontal ~align:Right [["1"; "213ad"; "3";]; ["4"; "5"; "6"]]
+(* let () = print_endline @@ table ~style:onlyIner [["1"; "213ad"; "3";]; ["4"; "5"; "6"]] *)
